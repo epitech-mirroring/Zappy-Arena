3c1525d2cdd18444f9312fad4e0618aa684bd156
RenardFute <axel.eckenberg@yahoo.fr>
2024-06-15 22:31:31 +0200

    fix: Fix password and cors errors

--- frontend/composables/Accounts.ts
+++ frontend/composables/Accounts.ts
@@ -1,8 +1,8 @@
 export const hashPassword = (password: string): string => {
     const salt = "EpitechNantesSupremacy";
-    const crypto = require('crypto');
-    const sha256 = crypto.createHash('sha256');
-    const sha384 = crypto.createHash('sha384');
-    const hashed = sha256(password + salt);
-    return sha384(hashed + salt).toString('hex');
+    const encoder = new TextEncoder();
+    let data = encoder.encode(password + salt);
+    const hash = crypto.subtle.digest('SHA-256', data);
+    data = encoder.encode(hash + salt);
+    return crypto.subtle.digest('SHA-384', data).toString();
 }
